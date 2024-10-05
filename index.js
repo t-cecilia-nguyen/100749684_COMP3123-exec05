3b1748b9496ad41b7f16de63bf81e6610867f1cf
Cecilia Nguyen <cecilia.t.nguyen@gmail.com>
2024-10-05 19:40:56 -0400

    completed lab 5

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const router = express.Router();
+
+app.use(express.json());
 
 /*
 - Create new html file name home.html 
@@ -8,14 +11,20 @@ const router = express.Router();
 - Return home.html page to client
 */
 router.get('/home', (req,res) => {
-  res.send('This is home router');
+  res.sendFile(__dirname + '/home.html');
 });
 
 /*
 - Return all details from user.json file to client as JSON format
 */
 router.get('/profile', (req,res) => {
-  res.send('This is profile router');
+  fs.readFile('user.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send({ message: 'Server Error' });
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
 });
 
 /*
@@ -37,16 +46,32 @@ router.get('/profile', (req,res) => {
         message: "Password is invalid"
     }
 */
-router.post('/login', (req,res) => {
-  res.send('This is login router');
+router.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  fs.readFile('user.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send({ message: 'Server Error' });
+    } else {
+      const user = JSON.parse(data);
+      if (user.username !== username) {
+        res.json({ status: false, message: 'User Name is invalid' });
+      } else if (user.password !== password) {
+        res.json({ status: false, message: 'Password is invalid' });
+      } else {
+        res.json({ status: true, message: 'User Is valid' });
+      }
+    }
+  });
 });
+
 
 /*
 - Modify /logout route to accept username as parameter and display message
     in HTML format like <b>${username} successfully logout.<b>
 */
 router.get('/logout', (req,res) => {
-  res.send('This is logout router');
+  const { username } = req.query;
+  res.send(`<b>${username} successfully logout.</b>`);
 });
 
 /*
@@ -54,7 +79,7 @@ Add error handling middleware to handle below error
 - Return 500 page with message "Server Error"
 */
 app.use((err,req,res,next) => {
-  res.send('This is error router');
+  res.status(500).send('Server Error');
 });
 
 app.use('/', router);
